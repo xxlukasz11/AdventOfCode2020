78b0e5cfd137af97dbb9739929542537056e4a9b
Lukasz Bozek <xxlukasz11@gmail.com>
2020-12-01 13:04:56 +0100

    Solve day 1

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <tuple>
 #include "../common/fileReader.h"
+
+std::pair<long, long> find2020Pair(const std::vector<long>& values) {
+	const long valuesSize = values.size();
+	for (long i = 0; i < valuesSize; ++i) {
+		for (long j = i + 1; j < valuesSize; ++j) {
+			const long val1 = values[i];
+			const long val2 = values[j];
+			if (val1 + val2 == 2020) {
+				return { val1, val2 };
+			}
+		}
+	}
+}
+
+std::tuple<long, long, long> find2020Triplet(const std::vector<long>& values) {
+	const long valuesSize = values.size();
+	for (long i = 0; i < valuesSize; ++i) {
+		for (long j = i + 1; j < valuesSize; ++j) {
+			for (long k = j + 1; k < valuesSize; ++k) {
+				const long val1 = values[i];
+				const long val2 = values[j];
+				const long val3 = values[k];
+				if (val1 + val2 + val3 == 2020) {
+					return { val1, val2, val3 };
+				}
+			}
+		}
+	}
+}
+
+void partOne(const std::vector<long>& values) {
+	const auto [val1, val2] = find2020Pair(values);
+	std::cout << val1 << " * " << val2 << " = " << val1 * val2 << std::endl;
+}
+
+void partTwo(const std::vector<long>& values) {
+	const auto [val1, val2, val3] = find2020Triplet(values);
+	std::cout << val1 << " * " << val2  << " * " << val3 <<
+		" = " << val1 * val2 * val3 << std::endl;
+}
 
 int main() {
 	common::FileReader reader("input.txt");
-	int totalFuel = 0;
+	std::vector<long> values;
 	for (std::string line; reader.nextLine(line);) {
-		const int value = std::stoi(line);
-		const int fuel = value / 3 - 2;
-		totalFuel += fuel;
+		const long value = std::stol(line);
+		values.push_back(value);
 	}
-	std::cout << "Fuel: " << totalFuel << std::endl;
+	partTwo(values);
 	return 0;
 }
