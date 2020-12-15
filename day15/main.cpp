d8a262794716bdfc6febbc821e01c0cf66833f39
Lukasz Bozek <xxlukasz11@gmail.com>
2020-12-15 06:39:07 +0100

    solve day 15

--- day15/main.cpp
+++ day15/main.cpp
@@ -1,31 +1,42 @@
 #include "../common/fileReader.h"
 #include <iostream>
 #include <vector>
-#include <string>
+#include <unordered_map>
 
 using DataType = std::vector<int>;
 
-DataType read() {
-	common::FileReader reader("input.txt");
-	DataType data;
-	for (std::string line; reader.nextLine(line);) {
-
+int lastNumberAtStep(const DataType& data, const int targetStep) {
+	std::unordered_map<int, int> registry;
+	for (int i = 0; i < data.size() - 1; ++i) {
+		registry[data[i]] = i + 1;
 	}
-	return data;
+
+	int lastNumber = data.back();
+	for (int currentIndex = data.size(); currentIndex < targetStep; ++currentIndex) {
+		const int lastOccurence = registry[lastNumber];
+		registry[lastNumber] = currentIndex;
+		if (lastOccurence == 0) {
+			lastNumber = 0;
+		}
+		else {
+			lastNumber = currentIndex - lastOccurence;
+		}
+	}
+	return lastNumber;
 }
 
 void partOne(const DataType& data) {
-
-	std::cout << "Part one: " << std::endl;
+	const int lastNumber = lastNumberAtStep(data, 2020);
+	std::cout << "Part one: " << lastNumber << std::endl;
 }
 
 void partTwo(const DataType& data) {
-
-	std::cout << "Part two: " << std::endl;
+	const int lastNumber = lastNumberAtStep(data, 30000000);
+	std::cout << "Part two: " << lastNumber << std::endl;
 }
 
 int main() {
-	const auto data = read();
+	DataType data{ 13,0,10,12,1,5,8 };
 	partOne(data);
 	partTwo(data);
 	return 0;
